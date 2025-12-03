5302a33db9c0ed80e342a70a11bfd018c7819a6b
lamnguyen231 <mysteryme2312@gmail.com>
2025-12-03 20:37:51 +0700

    feat: UI for UserList

---
+++ src/java/com/wmsmobile/dao/dbConfig.java
@@ -0,0 +1,47 @@
+package com.wmsmobile.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class dbConfig {
+
+    protected static Connection con = null;
+    private PreparedStatement ps = null; 
+    private ResultSet rs = null; 
+    
+    private String username = "root";
+    private String password = "2312004lamvt";
+    private String uri = "jdbc:mysql://localhost:3306/wms-mobile";
+    private String driver = "com.mysql.cj.jdbc.Driver";
+
+    public dbConfig() {
+        try {
+            Class.forName(driver);
+            getConnection(); 
+            System.out.println("Connection Successful");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(dbConfig.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Driver not found");
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            if (con == null || con.isClosed()) {
+                con = DriverManager.getConnection(uri, username, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static void main(String[] args) {
+        dbConfig db = new dbConfig();
+    }
+}
--- src/java/com/wmsmobile/model/User.java
+++ src/java/com/wmsmobile/model/User.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.wmsmobile.controller;
+package com.wmsmobile.model;
 
 /**
  *
  * @author PC
  */
-public class temp {
+public class User {
     
 }
